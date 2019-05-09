cb325b7a49fc7a2eab794d3bcb8d6eaee3d826ed
Priyanshu Tyagi <priyanshumain@gmail.com>
2019-05-09 23:27:51 +0530

    Add Note functionality

--- notes-app/app.js
+++ notes-app/app.js
@@ -28,9 +28,7 @@ yargs.command({
         }
     },
     handler : function (argv) {
-        console.log('Adding a note!', argv)
-        console.log('Title: ' + argv.title)
-        console.log('Body: ' + argv.body)
+        notes.addNote(argv.title, argv.body)
     }
 })
 
