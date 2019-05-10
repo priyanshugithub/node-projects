ce367c77d3b2069b5bf35ea5115c356bf9ac37a7
Priyanshu Tyagi <priyanshumain@gmail.com>
2019-05-10 09:59:56 +0530

    ES6 aside Arrow functions playground

--- playground/2-arrow-function.js
+++ playground/2-arrow-function.js
@@ -6,6 +6,20 @@
 //    return x * x
 // }
 
-const square = (x) => x * x 
+//const square = (x) => x * x 
 
-console.log(square(2))
+//console.log(square(2))
+
+const event = {
+    name : 'Birthday Party',
+    guestList : ['Priyanshu', 'Rajneesh', 'Sagar'],
+    printGuestList() {
+        console.log('Guest List for ' + this.name)
+
+        this.guestList.forEach((guest) => {
+            console.log(guest + ' is attending '+ this.name)
+        })
+    }
+}
+
+event.printGuestList()
