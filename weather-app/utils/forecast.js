4551cfc05651a72ed2f7745fc315e285fbe8336e
Priyanshu Tyagi <priyanshumain@gmail.com>
2019-05-11 23:54:59 +0530

    forecast.js

---
+++ weather-app/utils/forecast.js
@@ -0,0 +1,4 @@
+const request = require('request')
+
+const forecast = (latitude, longitude, callback) => {
+    const url = 'https://api.darksky.net/forecast/9d1465c6f3bb7a6c71944bdd8548d026/' + latitude + ',' + longitude
