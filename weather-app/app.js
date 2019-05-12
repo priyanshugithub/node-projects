a1defa3397ee318de68c0cf1efff8c77b8983af5
Priyanshu Tyagi <priyanshumain@gmail.com>
2019-05-12 19:04:11 +0530

    Parsing Weather API data as JSON

--- weather-app/app.js
+++ weather-app/app.js
@@ -3,5 +3,6 @@ const request = require('request')
 const url = 'https://api.darksky.net/forecast/ddb7333ef809753d00ef99196470edcf/37.8267,-122.4233'
 
 request({ url : url}, (error, response) => {
-    console.log(response)
+    const data = JSON.parse(response.body)
+    console.log(data)
 })
