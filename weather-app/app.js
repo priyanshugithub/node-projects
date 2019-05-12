a6f9290c5cfdbd0e8bcac1835f7bb629f34a0fa8
Priyanshu Tyagi <priyanshumain@gmail.com>
2019-05-12 19:26:02 +0530

    Adding more information to weather api

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,8 +1,8 @@
 const request = require('request')
 
-const url = 'https://api.darksky.net/forecast/ddb7333ef809753d00ef99196470edcf/37.8267,-122.4233'
+const url = 'https://api.darksky.net/forecast/ddb7333ef809753d00ef99196470edcf/37.8267,-122.4233?units=si'
 
 request({ url : url, json : true}, (error, response) => {
     //console.log(response.body.currently)
-    console.log('It is currently ' + response.body.currently.temperature + ' degrees out there. There is a ' + response.body.currently.precipProbability + '% chance of rain.')
+    console.log(response.body.daily.data[0].summary + ' It is currently ' + response.body.currently.temperature + ' degrees out there. There is a ' + response.body.currently.precipProbability + '% chance of rain.')
 })
