4997123d0bb0e3fbeb3f7d29c8f17e77a1a1cb36
Priyanshu Tyagi <priyanshumain@gmail.com>
2019-05-12 19:09:28 +0530

    Parsing using cleaner code

--- weather-app/app.js
+++ weather-app/app.js
@@ -2,7 +2,6 @@ const request = require('request')
 
 const url = 'https://api.darksky.net/forecast/ddb7333ef809753d00ef99196470edcf/37.8267,-122.4233'
 
-request({ url : url}, (error, response) => {
-    const data = JSON.parse(response.body)
-    console.log(data)
+request({ url : url, json : true}, (error, response) => {
+    console.log(response.body.currently)
 })
