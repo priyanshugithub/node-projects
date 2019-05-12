f91b6f119924ae060b2ce44f4ac49dd44110b6d0
Priyanshu Tyagi <priyanshumain@gmail.com>
2019-05-12 18:59:59 +0530

    Making HTTP requests

---
+++ weather-app/app.js
@@ -0,0 +1,7 @@
+const request = require('request')
+
+const url = 'https://api.darksky.net/forecast/ddb7333ef809753d00ef99196470edcf/37.8267,-122.4233'
+
+request({ url : url}, (error, response) => {
+    console.log(response)
+})
