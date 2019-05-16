a79681f44877b0a489fc5d1f7ad3cfcbca9a2a83
Priyanshu Tyagi <priyanshumain@gmail.com>
2019-05-16 23:11:45 +0530

    Serving up HTML and JSON

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -6,6 +6,20 @@ app.get('', (req, res) => {
     res.send('Hello express!')
 })
 
+app.get('/help', (req, res) => {
+    res.send('Help!!!!!!!')
+})
+
+app.get('/about', (req, res) => {
+    res.send('<h1>About</h1>')
+})
+
+app.get('/weather', (req, res) => {
+    res.send([{forecast: 'Forecast'},
+        {location: 'Location'
+    }])
+})
+
 app.listen(3000, () => {
     console.log('Server is up on port 3000!')
 })
