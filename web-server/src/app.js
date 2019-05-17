104dc3e363cd4b4b3d549cc5d11b21a029a293de
Priyanshu Tyagi <priyanshumain@gmail.com>
2019-05-17 23:43:36 +0530

    Advanced Templating using Partial Handlebars

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,12 +1,20 @@
 const path = require('path')
 const express =  require('express')
+const hbs = require('hbs')
 
 const app = express()
-const publicDirectory = path.join(__dirname, '../public')
-const viewsPath =  path.join(__dirname, '../templates')
 
+//Define paths for Express config
+const publicDirectory = path.join(__dirname, '../public')
+const viewsPath =  path.join(__dirname, '../templates/views')
+const partialsPath =  path.join(__dirname, '../templates/partials')
+
+//Setup handlebars engine and views location
 app.set('view engine', 'hbs')
 app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+//Setup static directory to serve
 app.use(express.static(publicDirectory))
 
 app.get('', (req, res) => {
@@ -18,13 +26,16 @@ app.get('', (req, res) => {
 
 app.get('/about', (req, res) => {
     res.render('about', {
+        title: 'About Me',
         name: 'Priyanshu Tyagi'
     })
 })
 
 app.get('/help', (req, res) => {
     res.render('help', {
-        message: 'HELP!!!'
+        message: 'This is the help page',
+        title: 'Help',
+        name: 'Priyanshu'
     })
 })
 app.get('/weather', (req, res) => {
