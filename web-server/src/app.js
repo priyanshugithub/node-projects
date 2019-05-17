780fb35c0aa39a0e69f1937de68ce5c2c86d8774
Priyanshu Tyagi <priyanshumain@gmail.com>
2019-05-17 23:52:05 +0530

    404 Error Request

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -45,6 +45,10 @@ app.get('/weather', (req, res) => {
     })
 })
 
+app.get('/help/*', (req, res) => {
+    res.send('Help article not found')
+})
+
 app.get('*', (req, res) => {
     res.send('My 404 Page')
 })
