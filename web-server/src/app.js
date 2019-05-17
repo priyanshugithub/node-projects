59186ecbe204674510ff966495b55e996341eff1
Priyanshu Tyagi <priyanshumain@gmail.com>
2019-05-17 23:22:00 +0530

    Setting Handlebar

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -3,8 +3,10 @@ const express =  require('express')
 
 const app = express()
 const publicDirectory = path.join(__dirname, '../public')
+const viewsPath =  path.join(__dirname, '../templates')
 
 app.set('view engine', 'hbs')
+app.set('views', viewsPath)
 app.use(express.static(publicDirectory))
 
 app.get('', (req, res) => {
