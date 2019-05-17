b536bff678d20a862cc0d966c6b16e9e5079cb6b
Priyanshu Tyagi <priyanshumain@gmail.com>
2019-05-17 23:01:49 +0530

    Setting Handlebar

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -8,9 +8,23 @@ app.set('view engine', 'hbs')
 app.use(express.static(publicDirectory))
 
 app.get('', (req, res) => {
-    res.render('index')
+    res.render('index', {
+        title: 'Weather App',
+        name: 'Priyanshu Tyagi'
+    })
 })
 
+app.get('/about', (req, res) => {
+    res.render('about', {
+        name: 'Priyanshu Tyagi'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        message: 'HELP!!!'
+    })
+})
 app.get('/weather', (req, res) => {
     res.send ({
         forecast: 'Forecast',
