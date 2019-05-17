0b0901bb9002e2582db04abe93144f1c128f7216
Priyanshu Tyagi <priyanshumain@gmail.com>
2019-05-17 22:50:16 +0530

    Setting Handlebars

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -7,6 +7,10 @@ const publicDirectory = path.join(__dirname, '../public')
 app.set('view engine', 'hbs')
 app.use(express.static(publicDirectory))
 
+app.get('', (req, res) => {
+    res.render('index')
+})
+
 app.get('/weather', (req, res) => {
     res.send ({
         forecast: 'Forecast',
