a5bb7d88f3300bbc5976162f34bc5ede03b6b166
Priyanshu Tyagi <priyanshumain@gmail.com>
2019-05-19 14:22:53 +0530

    Final Weather App Commit

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,18 +1,26 @@
 console.log('Client Side Javascript file is loaded!!!!')
 
-fetch('http://puzzle.mead.io/puzzle').then((response) => {
-    response.json().then((data) => {
-        console.log(data)
-    })
-})
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo= document.querySelector('#message-2')
 
-fetch('http://localhost:3000/weather?address=boston').then((response) => {
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const location = search.value
+
+    messageOne.textContent = 'Loading.....'
+    messageTwo.textContent = ''
+
+    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
     response.json().then((data) => {
         if(data.error) {
-            console.log(data.error)
+            messageOne.textContent = data.error
         } else {
-            console.log(data.location)
-            console.log(data.forecast)
-        }
+            messageOne.textContent = data.location
+            messageTwo.textContent = data.forecast
+            }
+        })
     })
 })
